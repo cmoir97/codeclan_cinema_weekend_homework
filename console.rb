e654e826b977db36ef16e832d640f2a77838304f
Cameron Moir <cameronjmoir@gmail.com>
2019-09-27 14:04:58 +0100

    find all tickets function added

--- console.rb
+++ console.rb
@@ -4,16 +4,22 @@ require_relative("models/ticket")
 
 require('pry')
 
-customer1 = Customer.new({'name' => 'Cameron', 'funds' => 50})
+customer1 = Customer.new({'name' => 'Cameron', 'funds' => 100})
+customer2 = Customer.new({'name' => 'Chris', 'funds' => 50})
 customer1.save()
+customer2.save()
 
 
 film1 = Film.new({'title' => 'There Will Be Blood', 'price' => 10})
+film2 = Film.new({'title' => 'Eraserhead', 'price' => 5})
 film1.save()
+film2.save()
 
 
 ticket1 = Ticket.new({'customer_id' => customer1.id(), 'film_id' => film1.id()})
+ticket2 = Ticket.new({'customer_id' => customer2.id(), 'film_id' => film2.id()})
 ticket1.save()
+ticket2.save()
 
 
 
